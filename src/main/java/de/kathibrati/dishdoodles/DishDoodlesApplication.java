fc63d10a72277b87f78131696bf1b79412238c6e
Kathleen Letsch <kathleen.letsch@kittelberger.de>
2024-05-13 17:16:22 +0200

    initial commit

---
+++ src/main/java/de/kathibrati/dishdoodles/DishDoodlesApplication.java
@@ -0,0 +1,13 @@
+package de.kathibrati.dishdoodles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DishDoodlesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DishDoodlesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/kathibrati/dishdoodles/DishDoodlesApplicationTests.java
@@ -0,0 +1,13 @@
+package de.kathibrati.dishdoodles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DishDoodlesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
