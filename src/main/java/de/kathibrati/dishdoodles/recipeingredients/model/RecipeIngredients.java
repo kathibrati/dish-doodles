0eb75181853e7a89bfe7a118efcd5907ddc4bfcb
Kathleen Letsch <kathleen.letsch@kittelberger.de>
2024-08-16 13:18:23 +0200

    wip

--- src/main/java/de/kathibrati/dishdoodles/recipeingredients/model/RecipeIngredients.java
+++ src/main/java/de/kathibrati/dishdoodles/recipeingredients/model/RecipeIngredients.java
@@ -1,24 +1,24 @@
-package de.kathibrati.dishdoodles.recipeingredients.model;
-
-import de.kathibrati.dishdoodles.ingredient.model.Ingredient;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-
-import java.util.List;
-
-@Entity
-public class RecipeIngredients {
-
-  @Id
-  @GeneratedValue
-  private Long id;
-
-  private List<Ingredient> ingredientId;
-
-  private Long recipeId;
-
-
-
-
-}
+//package de.kathibrati.dishdoodles.recipeingredients.model;
+//
+//import de.kathibrati.dishdoodles.ingredient.model.Ingredient;
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.Id;
+//
+//import java.util.List;
+//
+//@Entity
+//public class RecipeIngredients {
+//
+//  @Id
+//  @GeneratedValue
+//  private Long id;
+//
+//  private List<Ingredient> ingredientId;
+//
+//  private Long recipeId;
+//
+//
+//
+//
+//}
--- src/main/java/de/kathibrati/dishdoodles/units/model/Unit.java
+++ src/main/java/de/kathibrati/dishdoodles/units/model/Unit.java
@@ -1,14 +1,14 @@
-package de.kathibrati.dishdoodles.units.model;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-
-@Entity
-public class Unit {
-
-  @Id
-  @GeneratedValue
-  private Long id;
-
-}
+//package de.kathibrati.dishdoodles.units.model;
+//
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.Id;
+//
+//@Entity
+//public class Unit {
+//
+//  @Id
+//  @GeneratedValue
+//  private Long id;
+//
+//}
